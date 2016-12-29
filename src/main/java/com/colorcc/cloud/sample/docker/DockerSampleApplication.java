7b0a4870fa6ba2e9125af11be4c185448861ccf6
qintianjie <tianjieqin@126.com>
2016-12-29 11:27:05 +0800

    sampe web init

---
+++ src/main/java/com/colorcc/cloud/sample/docker/DockerSampleApplication.java
@@ -0,0 +1,12 @@
+package com.colorcc.cloud.sample.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerSampleApplication.class, args);
+	}
+}
---
+++ src/main/java/com/colorcc/cloud/sample/docker/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.colorcc.cloud.sample.docker.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+	@GetMapping("/say")
+	public String say(@RequestParam(name = "name", required = false) String name) {
+		if (name == null || name.trim().length() == 0) {
+			name = "world";
+		}
+
+		return "Hello " + name;
+	}
+}
